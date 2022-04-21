8efdbb571f1db0c08ac190fc69782443d0a26db0
Юсупова Светлана <sayusupova@edu.hse.ru>
2022-04-21 17:48:15 +0300

    Создала стартовое окно, с работающей кнопкой выхода и обновлением Json

--- LibraryApp.Core/Storage.cs
+++ LibraryApp.Core/Storage.cs
@@ -1,13 +1,133 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LibraryApp.Core
 {
-    class Storage
+    public class Storage
     {
+        private const string filePathUsers = "../../Data/Users.json";
+        private const string filePathAdmin = "../../Data/Admin.json";
+        private const string filePathBooks = "../../Data/Books.json";
 
+        public List<User> Users { get; private set; }
+        public List<BookInLibrary> Books { get; private set; }
+        public List<Admin> Admins { get; private set; }
+
+        public Storage()
+        {
+            Users = new List<User> { };
+            Books = new List<BookInLibrary> { };
+            Admins = new List<Admin> { };
+            /*ReadUsers();
+            ReadAdmin();
+            ReadBooks();*/
+        }
+
+        public void SaveUsers()
+        {
+            using (var sw = new StreamWriter(filePathUsers))
+            {
+                using (var jsonWriter = new JsonTextWriter(sw))
+                {
+                    jsonWriter.Formatting = Formatting.Indented;
+
+                    var serializer = new JsonSerializer()
+                    {
+                        TypeNameHandling = TypeNameHandling.Auto
+                    };
+
+                    serializer.Serialize(jsonWriter, Users);
+                }
+            }
+        }
+
+        private void ReadUsers()
+        {
+            using (var sr = new StreamReader(filePathUsers))
+            {
+                using (var jsonReader = new JsonTextReader(sr))
+                {
+                    var serializer = new JsonSerializer()
+                    {
+                        TypeNameHandling = TypeNameHandling.Auto
+                    };
+
+                    Users = serializer.Deserialize<List<User>>(jsonReader);
+                }
+            }
+        }
+
+        public void SaveBooks()
+        {
+            using (var sw = new StreamWriter(filePathBooks))
+            {
+                using (var jsonWriter = new JsonTextWriter(sw))
+                {
+                    jsonWriter.Formatting = Formatting.Indented;
+
+                    var serializer = new JsonSerializer()
+                    {
+                        TypeNameHandling = TypeNameHandling.Auto
+                    };
+
+                    serializer.Serialize(jsonWriter, Books);
+                }
+            }
+        }
+
+        private void ReadBooks()
+        {
+            using (var sr = new StreamReader(filePathBooks))
+            {
+                using (var jsonReader = new JsonTextReader(sr))
+                {
+                    var serializer = new JsonSerializer()
+                    {
+                        TypeNameHandling = TypeNameHandling.Auto
+                    };
+
+                    Books = serializer.Deserialize<List<BookInLibrary>>(jsonReader);
+                }
+            }
+        }
+
+        public void SaveAdmin()
+        {
+            using (var sw = new StreamWriter(filePathAdmin))
+            {
+                using (var jsonWriter = new JsonTextWriter(sw))
+                {
+                    jsonWriter.Formatting = Formatting.Indented;
+
+                    var serializer = new JsonSerializer()
+                    {
+                        TypeNameHandling = TypeNameHandling.Auto
+                    };
+
+                    serializer.Serialize(jsonWriter, Admins);
+                }
+            }
+        }
+
+        private void ReadAdmin()
+        {
+            using (var sr = new StreamReader(filePathAdmin))
+            {
+                using (var jsonReader = new JsonTextReader(sr))
+                {
+                    var serializer = new JsonSerializer()
+                    {
+                        TypeNameHandling = TypeNameHandling.Auto
+                    };
+
+                    Admins = serializer.Deserialize<List<Admin>>(jsonReader);
+                }
+            }
+        }
     }
 }
--- LibraryAppDesign/MainWindow.xaml.cs
+++ LibraryAppDesign/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LibraryApp.Core;
 
 namespace LibraryAppDesign
 {
@@ -23,6 +24,37 @@ namespace LibraryAppDesign
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        static Storage _storage = new Storage();
+        List<User> users = _storage.Users;
+        List<BookInLibrary> books = _storage.Books;
+        List<Admin> admins = _storage.Admins;
+
+        private void Admin(object sender, RoutedEventArgs e)
+        {
+            //для кнопки входа в админа и дальнейшие действия (в новом окне)
+
+        }
+
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            //для кнопки входа в пользователя и дальнейшие действия (в новом окне)
+
+        }
+
+        private void Autorisation(object sender, RoutedEventArgs e)
+        {
+            //для кнопки авторизации в пользователя
+
+        }
+
+        private void LogOut(object sender, RoutedEventArgs e)
+        {
+            _storage.SaveUsers();
+            _storage.SaveBooks();
+            _storage.SaveAdmin();
+            Close();
         }
     }
 }
