ad463fbb846e878affa2755c6e4cb381e99d4d3c
soniaermakova <saermakova@edu.hse.ru>
2022-06-25 22:23:17 +0300

    создано окно для удаления книги

---
+++ LibraryAppDesign/DeletingBookWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibraryAppDesign
+{
+    /// <summary>
+    /// Логика взаимодействия для DeletingBookWindow.xaml
+    /// </summary>
+    public partial class DeletingBookWindow : Window
+    {
+        public DeletingBookWindow(List<string> filters, string login)
+        {
+            InitializeComponent();
+            admin = login;
+            filters4Book = filters;
+            _storage = new Storage();
+
+            
+            BooksListBox.ItemsSource = filtersBooks;
+        }
+
+        string admin;
+        static Storage _storage = new Storage();
+        List<BookInLibrary> books = _storage.Books;
+
+        List<string> titlenames = new List<string> { };
+        List<string> authornames = new List<string> { };
+        string userlogin;
+        List<string> filters4Book;
+        List<BookInLibrary> filtersBooks = new List<BookInLibrary> { };
+
+        private void Apply_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TitleName_Initialized(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AuthorName_Initialized(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChooseBook_Initialized(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChooseBook_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Return_Click(object sender, RoutedEventArgs e)
+        {
+            new EditBookWindow(admin).Show();
+            Close();
+        }
+    }
+}
--- LibraryAppDesign/EditBookWindow.xaml.cs
+++ LibraryAppDesign/EditBookWindow.xaml.cs
@@ -38,6 +38,8 @@ namespace LibraryAppDesign
         private void Deletebook_Click(object sender, RoutedEventArgs e)
         {
             // для удаления книги
+            new DeletingBookWindow(new List<string> { "", ""}, admin).Show();
+            Close();
         }
 
         private void Addbook_Click(object sender, RoutedEventArgs e)
