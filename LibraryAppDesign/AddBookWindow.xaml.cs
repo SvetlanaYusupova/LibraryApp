397187413a13d0c59421947716e3498b472f403a
KatiaAronova <eiaronova@edu.hse.ru>
2022-06-26 21:11:14 +0300

    Добавлены новые компоненты в функцию добавления книги

--- LibraryAppDesign/AddBookWindow.xaml.cs
+++ LibraryAppDesign/AddBookWindow.xaml.cs
@@ -53,10 +53,16 @@ namespace LibraryAppDesign
                             {
                                 try
                                 {
-
-                                    _storage.Books.Add(new BookInLibrary(textBoxName.Text, textBoxAuthors.Text.Split(new string[] { ", "}, StringSplitOptions.None).ToList(), ageBox.SelectedItem.ToString(), textBoxDescription.Text, textBoxGenre.Text, int.Parse(textBoxNumberBook.Text), int.Parse(textBoxNumberBook.Text)));
-                                    _storage.SaveBooks();
-                                    MessageBox.Show("Книга создана!");
+                                    if (int.Parse(textBoxNumberBook.Text) > 0 & double.Parse(textBoxRaiting.Text) >= 0 & int.Parse(textBoxYear.Text) > 0 & int.Parse(textBoxPage.Text) > 0)
+                                    {
+                                        _storage.Books.Add(new BookInLibrary(textBoxName.Text, textBoxAuthors.Text.Split(new string[] { ", " }, StringSplitOptions.None).ToList(), ageBox.SelectedItem.ToString(), textBoxDescription.Text, textBoxGenre.Text, int.Parse(textBoxYear.Text), double.Parse(textBoxRaiting.Text), int.Parse(textBoxPage.Text), int.Parse(textBoxNumberBook.Text), int.Parse(textBoxNumberBook.Text)));
+                                        _storage.SaveBooks();
+                                        MessageBox.Show("Книга создана!");
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("Кол-во книг, рейтинг, год публикации и кол-во страниц должны быть больше 0.");
+                                    }
                                 }
                                 catch
                                 {
