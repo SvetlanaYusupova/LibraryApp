9abaf0bf4d20fcf8346a9e303994279c7909bfb2
Юсупова Светлана <sayusupova@edu.hse.ru>
2022-06-27 15:36:04 +0300

    Внедрение рек системы

---
+++ LibraryAppDesign/CBWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using LibraryApp.Core;
+using System;
+using System.Collections.Generic;
+//using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+
+namespace LibraryAppDesign
+{
+    /// <summary>
+    /// Логика взаимодействия для CBWindow.xaml
+    /// </summary>
+    public partial class CBWindow : Window
+    {
+        public CBWindow(string login, List<string> filters)
+        {
+            
+            _storage = Factory.GetInstance().Storage;
+            userLogin = login;
+            filtersCB = filters;
+            GetBooks();
+            InitializeComponent();
+            TitleName.Text = filters[0];
+            NumberBox.Text = filters[1];
+
+            if (!booksName.Contains(filters[0]) || filters[0] =="")
+            {
+                BooksListBox.ItemsSource = rec;
+            }
+            else if (filters[1] == "" || !numbers.Contains(int.Parse(filters[1])) )
+            {
+                MessageBox.Show("Выберите количество книг в рекомендации");
+                BooksListBox.ItemsSource = rec;
+            }
+            else
+            {
+
+                var args_r = new string[2] { filters[0], filters[1] };
+                string result = null;
+                result = RScriptRunner.RunFromCmd(@"../../Data" + @"\CBsourse.R",  @"C:\Program Files\R\R-4.2.1\bin\Rscript.exe", args_r);
+                rec.Add(result);
+                BooksListBox.ItemsSource = rec;
+
+            }
+
+            
+        }
+
+        string userLogin;
+        IStorage _storage;
+        List<string> filtersCB;
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        List<string> rec = new List<string> { };
+        List<string> booksName = new List<string> { };
+
+
+        public void GetBooks()
+        {
+            foreach (var item in _storage.GetBooks)
+            {
+                booksName.Add(item.GetBookName());
+            }
+        }
+
+        private void LogOut_Click(object sender, RoutedEventArgs e)
+        {
+            new WindowUser(userLogin).Show();
+            Close();
+        }
+
+        private void TitleName_Initialized(object sender, EventArgs e)
+        {
+            ComboBox BookName = sender as ComboBox;
+            foreach (var item in _storage.GetBooks)
+            {
+                BookName.Items.Add(item.GetBookName());
+
+            }
+        }
+
+        private void Apply_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> fil = new List<string> { TitleName.Text.ToString(), NumberBox.Text.ToString() };
+
+            new CBWindow(userLogin, fil).Show();
+            Close();
+        }
+
+        private void BookName_Initialized(object sender, EventArgs e)
+        {
+            
+
+            TextBlock TitleName = sender as TextBlock;
+
+            string book = TitleName.DataContext as string;
+
+            TitleName.Text = book;
+        }
+
+        private void NumberBox_Initialized(object sender, EventArgs e)
+        {
+            ComboBox NumberBox = sender as ComboBox;
+            foreach (var item in numbers)
+            {
+                NumberBox.Items.Add(item);   
+            }
+        }
+    }
+}
---
+++ LibraryAppDesign/Data/CBsourse.R
@@ -0,0 +1,134 @@
+library(tidyverse)
+library(readr)
+library(dplyr)
+library("rjson")
+library(jsonlite)
+library(reshape2)
+library(tidytext)
+library(LDAvis)
+library(topicmodels)
+
+
+args <- commandArgs()
+books <- fromJSON(txt="boooks.json")
+a = rownames(books)
+books = books %>% mutate(id = as.numeric(a))
+users <- fromJSON(txt="Users.json")
+users = users %>% select(Login, PastBooks)
+metadata_content_based = books %>% select(id, BookName, Author, Genre, PublishedYear, Rating, NumPages)
+
+#возраст фильма
+metadata_content_based$film_age = 2022 - metadata_content_based$PublishedYear
+metadata_content_based = metadata_content_based %>% select(-PublishedYear)
+
+#длина названия фильма
+metadata_content_based = metadata_content_based %>% mutate(tlLength = str_length(metadata_content_based$BookName))
+metadata_content_based$tlLength = ifelse(is.na(metadata_content_based$tlLength), 0, metadata_content_based$tlLength)
+
+
+
+word_counts <- metadata_content_based %>%
+  filter(!is.na(Genre)) %>%
+  count(BookName, Genre, sort = TRUE) %>%
+  ungroup()
+
+review_dtm <- word_counts %>%
+  cast_dtm(BookName, Genre, n)
+
+review_lda <- LDA(review_dtm, k = 5, control = list(seed = 12345))
+
+
+review_topics <- tidy(review_lda, matrix = "beta")
+
+review_top_terms <- review_topics %>%
+  group_by(topic) %>%
+  top_n(10, beta) %>%
+  ungroup() %>%
+  arrange(topic, -beta)
+
+
+
+
+review_tags <- review_topics %>% rename(Genre = term)
+
+#Каждому тегу выбирается только одна группа с наибольшим бета
+tags_best_beta <- review_tags %>% 
+  group_by(Genre) %>% 
+  summarise(max_beta = max(beta))
+
+review_tags <- review_tags %>% 
+  filter(beta %in% tags_best_beta$max_beta)
+
+metadata_tags = books %>% select(BookName, Genre, Rating)
+metadata_tags_topics <- metadata_tags %>% 
+  left_join(review_tags) 
+
+metadata_tags_topics$topic = replace_na(metadata_tags_topics$topic, 0)
+
+metadata_tags_topics2 = metadata_tags_topics %>% group_by(BookName, topic) %>% summarise(n = n())
+metadata_tags_topics2 = metadata_tags_topics2 %>% group_by(BookName) %>% mutate(maxn = max(n))
+metadata_tags_topics2 = metadata_tags_topics2 %>% filter(maxn == n)
+metadata_tags_topics2 = metadata_tags_topics2 %>% select(BookName, topic)
+
+metadata_tags_topics2 = mutate(metadata_tags_topics2, g1 = 1)
+metadata_tags_topics2 = metadata_tags_topics2 %>% pivot_wider(names_from = topic, values_from = g1, values_fill = 0)
+metadata_content_based = right_join(metadata_content_based, metadata_tags_topics2)
+
+
+
+
+
+
+
+metadata_content_based = metadata_content_based %>% select(-Genre, -Author)
+content_based2 = metadata_content_based
+content_based2 = select(content_based2, -BookName)
+
+rownames = content_based2$id
+content_based2 = content_based2 %>% dplyr::select(-id)
+rownames(content_based2) = rownames
+sim = lsa::cosine(t(as.matrix(content_based2)))
+
+
+content_based <- function(name_book, number)
+{
+  
+ 
+  
+  film = books %>% filter(BookName == name_book)
+  
+  
+  if (count(film) == 0) {
+    books = books %>%  arrange(-Rating) %>%  select(BookName)
+    recommend = head(books, n = number)
+    
+    
+  } else {
+    
+    diag(sim) = 0
+    
+    simCut = sim[,as.character(film$id)]
+    mostSimilar = head(sort(simCut, decreasing = T), n = number)
+    a = which(simCut %in% mostSimilar, arr.ind = TRUE, useNames = T)
+    index = arrayInd(a, .dim = dim(sim))
+    result = rownames(sim)[index[,1]]
+    
+    mostSimilar = data.frame(id = as.numeric(result), similar = simCut[index])
+    recommend = mostSimilar %>% left_join(books) %>% select(BookName, similar) %>% arrange(-similar)
+    recommend = select(recommend, BookName)
+    recommend = head(recommend, n = number)
+  }
+  #reccomend = reccomend
+  as.list(recommend[1])
+  #head(recommend, n = number)
+}
+
+a = content_based(args[1],as.numeric(args[2]))$BookName
+
+
+s = NULL
+for (variable in a) {
+  s = paste(s,variable, sep = ",")
+}
+s = str_remove(s, ",")
+s
