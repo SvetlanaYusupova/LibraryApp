f0989f6a2b337f7b19d40a2176550ce7b939ec43
Юсупова Светлана <sayusupova@edu.hse.ru>
2022-06-27 15:44:14 +0300

    s

--- LibraryApp.Core/JSONStorage.cs
+++ LibraryApp.Core/JSONStorage.cs
@@ -20,6 +20,26 @@ namespace LibraryApp.Core
             _admins = new Repository<Admin>("../../Data/Admin.json");
             _books = new Repository<BookInLibrary>("../../Data/boooks.json");
             _notifications = new Repository<Notification>("../../Data/Notifications.json");
+            foreach (var user in GetUsers)
+            {
+                user.DeleteMessages();
+                foreach (var booking in user.GetOrderBook())
+                {
+                    if ((DateTime.Today - booking.GetEndDate()).Duration().Days < 3)
+                    {
+                        user.AddBookingMessage(booking);
+                    }
+                }
+
+                foreach (var taken in user.GetTakenBooks())
+                {
+                    if ((DateTime.Today - taken.GetEndDate()).Duration().Days < 7)
+                    {
+                        user.AddTakenMessage(taken);
+                    }
+                }
+            }
+            Save();
         }
 
         public List<User> GetUsers => _users.GetCollection;
